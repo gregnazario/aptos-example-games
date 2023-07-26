40fc446573c02ea85356280ae221b349d3e64ca7
Greg Nazario <greg@gnazar.io>
2023-07-26 13:06:07 -0700

    Add IC

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,30 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { RiseWallet } from "@rise-wallet/wallet-adapter";
-import { PetraWallet } from "petra-plugin-wallet-adapter";
-import { MartianWallet } from "@martianwallet/aptos-wallet-adapter";
-import { PontemWallet } from "@pontem/wallet-adapter-plugin";
-import { TrustWallet } from "@trustwallet/aptos-wallet-adapter";
+import {RiseWallet} from "@rise-wallet/wallet-adapter";
+import {PetraWallet} from "petra-plugin-wallet-adapter";
+import {MartianWallet} from "@martianwallet/aptos-wallet-adapter";
+import {PontemWallet} from "@pontem/wallet-adapter-plugin";
+import {TrustWallet} from "@trustwallet/aptos-wallet-adapter";
 import {
     AptosWalletAdapterProvider,
 } from "@aptos-labs/wallet-adapter-react";
-import { FewchaWallet } from "fewcha-plugin-wallet-adapter";
-import { MSafeWalletAdapter } from "msafe-plugin-wallet-adapter";
-import { WelldoneWallet } from "@welldone-studio/aptos-wallet-adapter";
-import { NightlyWallet } from "@nightlylabs/aptos-wallet-adapter-plugin";
+import {FewchaWallet} from "fewcha-plugin-wallet-adapter";
+import {MSafeWalletAdapter} from "msafe-plugin-wallet-adapter";
+import {WelldoneWallet} from "@welldone-studio/aptos-wallet-adapter";
+import {NightlyWallet} from "@nightlylabs/aptos-wallet-adapter-plugin";
+import {IdentityConnectWallet} from '@identity-connect/wallet-adapter-plugin';
 
+const icDappId = '56746ba8-b4e1-4ddf-9c59-3b406b5b5e2a';
 
-const wallets = [new PetraWallet(), new MartianWallet(), new PontemWallet(), new RiseWallet(), new FewchaWallet(), new TrustWallet(), new MSafeWalletAdapter(), new WelldoneWallet(), new NightlyWallet()];
+const wallets = [
+    new IdentityConnectWallet(icDappId),
+    new PetraWallet(),
+    new MartianWallet(),
+    new PontemWallet(),
+    new RiseWallet(),
+    new FewchaWallet(),
+    new TrustWallet(),
+    new MSafeWalletAdapter(),
+    new WelldoneWallet(),
+    new NightlyWallet()
+];
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-      <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
-          <App />
-      </AptosWalletAdapterProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
+            <App/>
+        </AptosWalletAdapterProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
