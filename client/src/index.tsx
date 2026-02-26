f90b5ec4cb346a8ec5a74acf2e576c674343174c
Greg Nazario <greg@gnazar.io>
2026-02-26 17:36:00 -0500

    chore: set up Biome for linting and formatting

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {PetraWallet} from "petra-plugin-wallet-adapter";
-import {
-    AptosWalletAdapterProvider,
-} from "@aptos-labs/wallet-adapter-react";
-import {Buffer as BufferPolyFill} from 'buffer';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+import { Buffer as BufferPolyFill } from "buffer";
+import { PetraWallet } from "petra-plugin-wallet-adapter";
+import App from "./App";
 
 window.Buffer = BufferPolyFill;
 
-const wallets = [
-    new PetraWallet(),
-];
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
+const wallets = [new PetraWallet()];
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    <React.StrictMode>
-        <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
-            <App/>
-        </AptosWalletAdapterProvider>
-    </React.StrictMode>
+	<React.StrictMode>
+		<AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
+			<App />
+		</AptosWalletAdapterProvider>
+	</React.StrictMode>,
 );
